4e08318851be1c23b79320f92640293613675ad5
Harsh Raj <131889619+RajHarsh27@users.noreply.github.com>
2023-04-28 09:35:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,104 @@
-let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
-let timerDisplay = document.querySelector('.timerDisplay');
-let time = null;
+let btn1 = document.getElementById("start_btn");
+let btn2 = document.getElementById("stop_btn");
+let btn3 = document.getElementById("reset_btn");
+let timer_text = document.getElementById("timer_text");
 
-document.getElementById('startTimer').addEventListener('click', ()=>{
-    if(time!==null){
-        clearInterval(time);
-    }
-    time = setInterval(displayTimer,10);
-});
+//add event listener to all buttons
+btn1.addEventListener("click" , Start);
+btn2.addEventListener("click" , Stop);
+btn3.addEventListener("click" , Reset);
 
-document.getElementById('pauseTimer').addEventListener('click', ()=>{
-    clearInterval(time);
-});
+let check = false;
+let check_for_start = false;
+//Define Start function
+let milliSeconds = 0;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let event_value;
+let ms, s , m , h;
 
-document.getElementById('resetTimer').addEventListener('click', ()=>{
-    clearInterval(time);
-    [milliseconds,seconds,minutes,hours] = [0,0,0,0];
-    timerDisplay.innerHTML = '00 : 00 : 00 : 000 ';
-});
+function Start() {
+if(check_for_start === true) return;
 
-function displayTimer(){
-    milliseconds+=10;
-    if(milliseconds == 1000){
-        milliseconds = 0;
+check = true;
+check_for_start = true;
+event_value = setInterval(function(){
+    milliSeconds += 5;
+    if(milliSeconds === 1000) {
+        milliSeconds = 0;
         seconds++;
-        if(seconds == 60){
-            seconds = 0;
-            minutes++;
-            if(minutes == 60){
-                minutes = 0;
-                hours++;
-            }
-        }
     }
- let h = hours < 10 ? "0" + hours : hours;
- let m = minutes < 10 ? "0" + minutes : minutes;
- let s = seconds < 10 ? "0" + seconds : seconds;
- let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
- timerDisplay.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
+    if(seconds === 60) {
+        seconds = 0;
+        minutes++;
+    }
+    if(minutes === 60) {
+        minutes = 0;
+        hours++;
+    }
+    if(hours === 24) {
+        Reset();
+    }
+
+    //-> change to string <-
+
+    //for milliseconds
+    if(milliSeconds < 10) {
+        ms = "00" + milliSeconds;
+    }
+    else if(milliSeconds < 100) {
+        ms = "0" + milliSeconds;
+    }
+    else {
+        ms = milliSeconds;
+    }
+
+    // for seconds
+    if(seconds < 10) {
+        s = "0" + seconds;
+    }
+    else {
+        s = seconds;
+    }
+
+    // for minutes
+    if(minutes < 10) {
+        m = "0" + minutes;
+    }
+    else {
+        m = minutes;
+    }
+
+    // for hours
+    if(hours < 10) {
+        h = "0" + hours;
+    }
+    else {
+        h = hours;
+    }
+
+    timer_text.innerHTML = h + " : " + m + " : " + s + " : " + ms;
+},5)
+}
+
+//Define Stop function
+function Stop() {
+if(check === false) return;
+clearInterval(event_value);
+start_btn.innerText = "RESUME";
+check_for_start = false;
+}
+
+//Define Reset function
+function Reset() {
+check = false;
+check_for_start = false;
+milliSeconds = 0;
+seconds = 0;
+minutes = 0;
+hours = 0;
+timer_text.innerHTML = "00 : 00 : 00 : 000";
+clearInterval(event_value);
+start_btn.innerText = "START";
 }
